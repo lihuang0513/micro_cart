1cef3717117397330d8cd56cf0537c468172362c
忽忽 <517306208@qq.com>
2024-12-25 14:18:55 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"git.imooc.com/coding-447/cart/common"
+	"git.imooc.com/coding-447/cart/domain/repository"
+	service2 "git.imooc.com/coding-447/cart/domain/service"
+	"git.imooc.com/coding-447/cart/handler"
+	cart "git.imooc.com/coding-447/cart/proto/cart"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/registry"
+	consul2 "github.com/micro/go-plugins/registry/consul/v2"
+	ratelimit "github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2"
+	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+	"github.com/opentracing/opentracing-go"
+)
+
+var QPS = 100
+
+func main() {
+	//配置中心
+	consulConfig, err := common.GetConsulConfig("172.24.0.2", 8500, "/micro/config")
+	if err != nil {
+		log.Error(err)
+	}
+	//注册中心
+	consul := consul2.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"172.24.0.2:8500",
+		}
+	})
+
+	//链路追踪
+	t, io, err := common.NewTracer("go.micro.service.cart", "172.24.0.4:6831")
+	if err != nil {
+		log.Error(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)
+
+	//数据库连接
+	mysqlInfo := common.GetMysqlFromConsul(consulConfig, "mysql")
+	fmt.Println(mysqlInfo)
+
+	//创建数据库连接
+	//db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Pwd+"@/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		mysqlInfo.User, mysqlInfo.Pwd, mysqlInfo.Host, mysqlInfo.Port, mysqlInfo.Database)
+
+	// 连接数据库
+	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Error(err)
+	}
+	defer db.Close()
+	//禁止副表
+	db.SingularTable(true)
+
+	//第一次初始化
+	//err = repository.NewCartRepository(db).InitTable()
+	//if err != nil {
+	//	log.Error(err)
+	//}
+
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.service.cart"),
+		micro.Version("latest"),
+		//暴露的服务地址
+		micro.Address("0.0.0.0:8090"),
+		//注册中心
+		micro.Registry(consul),
+		//链路追踪
+		micro.WrapHandler(opentracing2.NewHandlerWrapper(opentracing.GlobalTracer())),
+		//添加限流
+		micro.WrapHandler(ratelimit.NewHandlerWrapper(QPS)),
+	)
+
+	// Initialise service
+	service.Init()
+
+	cartDataService := service2.NewCartDataService(repository.NewCartRepository(db))
+
+	// Register Handler
+	cart.RegisterCartHandler(service.Server(), &handler.Cart{CartDataService: cartDataService})
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
